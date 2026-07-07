05d63c7cb03c79c8e13f3291433f148529296ec7
CPRINTSRL <cntstampa@gmail.com>
2026-07-07 12:03:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import clientiRoutes from './routes/clienti.js';
 import ordiniRoutes from './routes/ordini.js';
+import categorieRoutes from './routes/categorie.js';
 
 dotenv.config();
 
@@ -19,5 +20,6 @@ app.get('/health', (req, res) => res.json({ status: 'ok', service: 'C PRINT Gest
 app.use('/api/auth', authRoutes);
 app.use('/api/clienti', clientiRoutes);
 app.use('/api/ordini', ordiniRoutes);
+app.use('/api/categorie', categorieRoutes);
 
 app.listen(PORT, () => console.log(`C PRINT Gestionale in ascolto su porta ${PORT}`));
